fa31e5fdccf28318f1787c0d3a1d6964a73e15bc
Rodrigo Banci <rblima0@gmail.com>
2017-08-08 17:20:37 -0300

    Organizando estrutura, adicionando API em NODE para Servidor

---
+++ projeto/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      titulo: 'Searchall',
+      fotos: [
+        {
+          url: 'https://cdn.pixabay.com/photo/2016/10/22/17/46/scotland-1761292_960_720.jpg',
+          titulo: 'Paisagem'
+        },
+        {
+          url: 'http://desviantes.blob.core.windows.net/desviantes/production/media/uploads/pedra_do_c%C3%A9u_-_guaratinga.jpg',
+          titulo: 'Montanha'
+        }
+      ] 
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
